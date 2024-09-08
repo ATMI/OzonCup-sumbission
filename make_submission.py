7d1f396fc747971e441f19336572d537c4aabb63
a <artem.myasoedov@gmail.com>
2024-09-08 11:21:03 +0300

    Added separate thread to load images

--- make_submission.py
+++ make_submission.py
@@ -1,8 +1,11 @@
 import os
+import time
 import numpy as np
 import onnxruntime as ort
 
 from PIL import Image
+from threading import Thread
+from queue import Queue
 
 TEST_IMAGES_DIR = "./data/test/"
 SUBMISSION_PATH = "./data/submission.csv"
@@ -11,7 +14,13 @@ MODEL_PATH = "./model.onnx"
 
 def load_image(path):
 	image = Image.open(path)
-	image = image.convert("RGB")
+	if image.mode in ("RGBA", "LA") or (image.mode == "P" and "transparency" in image.info):
+		image = image.convert("RGBA")
+		background = Image.new("RGB", image.size, (255, 255, 255))
+		background.paste(image, mask=image.split()[3])
+		image = background
+	else:
+		image = image.convert("RGB")
 	image = image.resize((384, 384), Image.BILINEAR)
 
 	image = np.array(image, dtype=np.float32)
@@ -26,54 +35,57 @@ def load_image(path):
 	return image
 
 
+def load_images(images_dir: str, queue: Queue):
+	all_image_names = os.listdir(images_dir)
+
+	for i, image_name in enumerate(all_image_names):
+		try:
+			image_path = os.path.join(images_dir, image_name)
+			image = load_image(image_path)
+			queue.put((image_name, image))
+		except Exception as e:
+			# TODO: proper exception handling
+			print(e)
+
+	queue.put(None)
+
+
 if __name__ == "__main__":
-	all_image_names = os.listdir(TEST_IMAGES_DIR)
+	image_queue = Queue(2)
+	loader_thread = Thread(target=load_images, args=(TEST_IMAGES_DIR, image_queue))
+	loader_thread.start()
+
 	all_preds = []
 
 	session = ort.InferenceSession(MODEL_PATH)
 	input_name = session.get_inputs()[0].name
 	output_name = session.get_outputs()[0].name
 
-	for i, image_name in enumerate(all_image_names):
-		image = None
-		output = None
-		pred = None
+	start_time = time.time()
 
-		try:
-			image_path = os.path.join(TEST_IMAGES_DIR, image_name)
-			image = load_image(image_path)
+	while True:
+		data = image_queue.get()
+		if data is None:
+			break
 
-			output = session.run([output_name], {input_name: image})
-			output = output[0]
+		image_name, image = data
+		output = session.run([output_name], {input_name: image})
+		output = output[0]
 
-			# pred = np.argmax(output, axis=1)
-			pred = output > 0.5
-			pred = pred.item()
-			pred = int(pred)
+		pred = output > 0.5
+		pred = pred.item()
+		pred = int(pred)
+		pred = (image_name, pred)
 
-			all_preds.append(pred)
-		except Exception as e:
-			image_shape = None
-			image_min = None
-			image_max = None
+		all_preds.append(pred)
 
-			try:
-				image_shape = image.shape
-				image_min = image.min()
-				image_max = image.max()
-			except:
-				pass
-
-			raise RuntimeError(
-				f"\n[i] {i}"
-				f"\n[image name] {image_name}"
-				f"\n[image shape] {image_shape}"
-				f"\n[image min; max] {image_min}; {image_max}"
-				f"\n[output] {output}"
-				f"\n[error] {e}"
-			)
+	end_time = time.time()
+	exec_time = end_time - start_time
+	print(exec_time)
 
 	with open(SUBMISSION_PATH, "w") as f:
 		f.write("image_name\tlabel_id\n")
-		for name, cl_id in zip(all_image_names, all_preds):
+		for name, cl_id in all_preds:
 			f.write(f"{name}\t{cl_id}\n")
+
+	loader_thread.join()
